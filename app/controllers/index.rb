293d1e41020b74430031df9d031659393379f54e
Michael Angelo <yamikamisama@gmail.com>
2015-02-12 05:22:09 -0500

    created routes for all filters

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -11,6 +11,54 @@ get '/' do
   erb :index
 end
 
+#FAVORITES SHOW PAGE
+get '/favorites' do
+  @movies = Movie.where(favorite: true)
+  erb :index
+end
+
+#CRITIC RATING SHOW PAGE
+get '/critic-rating' do
+  @movies = Movie.order(critics_score: :desc )
+  erb :index
+end
+
+#AUDIENCE RATING SHOW PAGE
+get '/audience-rating' do
+  @movies = Movie.order(audience_score: :desc )
+  erb :index
+end
+
+#MOVIE RATING SHOW PAGE
+get '/movie-rating' do
+  @movies = Movie.order(:mpaa_rating)
+  erb :index
+end
+
+#RELEASE DATE PAGE
+get '/release-date' do
+  @movies = Movie.order(:release_date)
+  erb :index
+end
+
+#YEAR SHOW PAGE
+get '/year' do
+  @movies = Movie.order(year: :desc )
+  erb :index
+end
+
+#RUNTIME SHOW PAGE
+get '/runtime' do
+  @movies = Movie.order(runtime: :desc )
+  erb :index
+end
+
+#PAST FILTERS SHOW PAGE
+get '/filter/:id' do |id|
+  @filters = Filter.where(user_id: id)
+  erb :'filter/past_filters'
+end
+
 #UPDATE TO MOVIE FAVORITE BOOLEAN
 put '/:id' do |id|
   if request.xhr?
@@ -19,24 +67,6 @@ put '/:id' do |id|
   else
     redirect '/'
   end
-end
-
-#MOVIE SHOW PAGE
-# get '/movie/:id' do |id|
-#   @movie = Movie.find(id)
-#   erb :single_movie
-# end
-
-#FAVORITE MOVIE SHOW PAGE
-get '/favorites/:id' do |id|
-  @movies = Movie.where(user_id: id)
-  erb :favorites
-end
-
-#PAST FILTERS SHOW PAGE
-get '/filter/:id' do |id|
-  @filters = Filter.where(user_id: id)
-  erb :'filter/past_filters'
 end
 
 #FILTER SHOW PAGE BASED ON DATE AND RATING
@@ -57,4 +87,3 @@ post '/filter' do
   @filter = Filter.create(params[:filter])
   redirect "/filter/#{@filter.release_date}/#{@filter.critic_rating}"
 end
-
