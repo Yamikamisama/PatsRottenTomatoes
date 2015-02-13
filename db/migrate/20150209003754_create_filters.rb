f1e89361e36ab3903202946828ea49ff2c112526
Michael Angelo <yamikamisama@gmail.com>
2015-02-13 00:36:59 -0500

    remove unneeded filter migration

--- db/migrate/20150209003754_create_filters.rb
+++
@@ -1,9 +0,0 @@
-class CreateFilters < ActiveRecord::Migration
-  def change
-  	create_table :filters do |t|
-  		t.string :release_date
-  		t.string :critic_rating
-  		t.references :user
-  	end
-  end
-end
